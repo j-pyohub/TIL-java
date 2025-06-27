3871214516af7dbb03038a3ef0b7d2b9581991b7
pyosj <pyosuejung@gmail.com>
2025-06-28 04:46:36 +0900

    변수(changse) commit

---
+++ src/java-start/array/Array1.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        // 학생의 점수를 출력하는 프로그램
+
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 70;
+        int student4 = 60;
+        int student5 = 50;
+
+        System.out.println("학생1 점수: " + student1);
+        System.out.println("학생2 점수: " + student2);
+        System.out.println("학생3 점수: " + student3);
+        System.out.println("학생4 점수: " + student4);
+        System.out.println("학생5 점수: " + student5);
+
+        // 학생을 몇백명 추가해야 하는 상황이 오면, 손으로 직접 적는 건 불가능
+        // 이때는 배열을 사용해서 추가할 수 있음
+    }
+}
