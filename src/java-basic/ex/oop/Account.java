5f42814d6209c3379f753356c6a9c01b46669285
pyosj <pyosuejung@gmail.com>
2025-07-11 15:20:51 +0900

    객체지향 연습문제 commit

--- src/java-basic/ex/oop/Account.java
+++ src/java-basic/ex/oop/Account.java
@@ -1,5 +1,18 @@
 package ex.oop;
 
 public class Account {
+    int balance = 0; //잔액
 
+    void deposit(int amount) { // 입금 매서드
+        balance += amount;
+    }
+
+    void withdraw(int amount) {// 출금 매서드
+        if (balance - amount < 0) {
+            System.out.println("잔액 부족");
+            System.out.println("잔고: " + balance);
+        } else {
+            balance -= amount;
+        }
+    }
 }
