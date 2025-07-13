2d255f65bcd82acae1e72d8c1a7db484324e8c75
pyosj <pyosuejung@gmail.com>
2025-07-13 12:45:26 +0900

    생성자3 commit

--- src/java-basic/construct/MemberConstruct.java
+++ src/java-basic/construct/MemberConstruct.java
@@ -5,6 +5,11 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); //변경 //this는 생성자 코드의 첫줄에서만 작성 가능
+    }
+
     //생성자 이름 = 클래스 이름 / 반환타입x
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name = " + name + ", age = " + age + ", grage = " + grade);
