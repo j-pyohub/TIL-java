7947b658129be75bba2daa7ec9fca1b7f0ed2dd0
pyosj <pyosuejung@gmail.com>
2025-07-14 15:24:16 +0900

    접근제어자 commit

---
+++ src/java-basic/access/a/AccessInnetMain.java
@@ -0,0 +1,23 @@
+package access.a;
+
+public class AccessInnetMain {
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+        //public 호출 가능
+        data.publicField = 1;
+        data.publicMethod();
+
+        //같은 패키지 default 호출 가능
+        data.defaultField = 2;
+        data.defaultMethod();
+
+        //private 호출 불가
+        //data.privateField = 3;
+        //data.privateMethod();
+
+
+        // innerAccess가 public이므로 내부호출 가능
+        data.innerAccess();
+    }
+}
