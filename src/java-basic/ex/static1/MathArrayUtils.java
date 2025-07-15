7c940602d156a9a45c2a936796402d63826db786
pyosj <pyosuejung@gmail.com>
2025-07-15 14:58:53 +0900

    static2, 연습문제 commit

---
+++ src/java-basic/ex/static1/MathArrayUtils.java
@@ -0,0 +1,40 @@
+package ex.static1;
+
+public class MathArrayUtils {
+
+    private MathArrayUtils() {
+        // 인스턴스 생성을 막는다
+    }
+
+    public static int sum(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static double average(int[] values) {
+        return (double) sum(values) / values.length;
+    }
+
+    public static int min(int[] values) {
+        int minValue = values[0];
+        for (int value : values) {
+            if (value < minValue) {
+                minValue = value;
+            }
+        }
+        return minValue;
+    }
+
+    public static int max(int[] values) {
+        int maxValue = values[0];
+        for (int value : values) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+}
