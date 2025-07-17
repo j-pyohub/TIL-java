86ff0c7f998d2f59b1494c869a8549cbbb648f85
pyosj <pyosuejung@gmail.com>
2025-07-17 10:43:20 +0900

    final commit

---
+++ src/java-basic/final1/FieldInit.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class FieldInit {
+    
+    static final int CONST_VALUE = 10; //상수는 관례상 대문자+언더스코어 사용
+    final int value = 10;
+    
+    //이미 final에 초기값이 있는 경우에는 생성자(generate) 못 만듦
+}
